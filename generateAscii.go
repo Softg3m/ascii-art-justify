d8693fedbcdff74c72ff8dd62ad71b529c52b3ff
holadele <hikmaholadele14@gmail.com>
2026-03-30 11:55:53 +0100

    feat: handle Ascii Representation

---
+++ generateAscii.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"os"
+	"strings"
+)
+
+func GenerateAscii(text, banner string) string {
+	data, err := os.ReadFile("banners/" + banner + ".txt")
+	if err != nil {
+		return "Error reading banner file"
+	}
+
+	fileLines := strings.Split(string(data), "\n")
+	result := ""
+	words := strings.Split(text, "\n")
+
+	for _, word := range words {
+		for i := 1; i <= 8; i++ {
+			for _, char := range word {
+				asciiIndex := int(char) - 32
+				start := asciiIndex * 9
+
+				if start+i >= len(fileLines) {
+					continue
+				}
+
+				result += fileLines[start+i]
+			}
+			result += "\n"
+		}
+	}
+	return result
+}
