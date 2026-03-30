a44152d7e2c01400a0df2b4752611fcdb6e441d2
holadele <hikmaholadele14@gmail.com>
2026-03-30 11:48:39 +0100

    feat: first main commit

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		Usage()
+		return
+	}
+
+	if strings.HasPrefix(args[0], "--align=") {
+		filetype := strings.TrimPrefix(args[0], "--align=")
+
+		if filetype == "" {
+			Usage()
+			return
+		}
+
+		text := args[1]
+		banner := "standard"
+
+		if len(args) == 3 {
+			banner = args[2]
+		}
+		ascii := GenerateAscii(text, banner)
+		fmt.Print(ascii)
+
+	} else {
+		text := args[0]
+		banner := "standard"
+
+		if len(args) == 2 {
+			banner = args[1]
+		}
+		ascii := GenerateAscii(text, banner)
+		fmt.Println(ascii)
+	}
+
+}
+
+func Usage() {
+	fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]")
+	fmt.Println()
+	fmt.Println("Example: go run . --align=right something standard")
+}
